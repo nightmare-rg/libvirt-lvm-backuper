9c06349beac752ccea1138e96e86adfda63de41e
joerg <nightmare@rising-gods.de>
2013-12-11 16:02:25 +0100

    added exclude lv feature

--- lvm-backuper.sh
+++ lvm-backuper.sh
@@ -3,7 +3,7 @@
 #
 #          FILE: lvm-backuper.sh
 # 
-#         USAGE: ./lvm-backuper.sh vm vmname -d /tmp/ [-ssh "-p 22 user@example.com:/path/to/dest"] [-ts +%F]
+#         USAGE: ./lvm-backuper.sh vm vmname -d /tmp/ [-ssh "-p 22 user@example.com:/path/to/dest"] [-ts +%F] [-ex "storage01,storage02"]
 #
 #        AUTHOR: Jörg Stewig (nightmare@rising-gods.de), 
 #===============================================================================
@@ -14,11 +14,12 @@
 
 function usage
 {
-    echo -e "\nusage: $0 -vm vmname -d /tmp/ [-ssh \"-p 22 user@example.com:/path/to/dest\"] [-ts +%F] \n"
+    echo -e "\nusage: $0 -vm vmname -d /tmp/ [-ssh \"-p 22 user@example.com:/path/to/dest\"] [-ts +%F] [-ex \"storage01,storage02\"] \n"
     echo -e "-vm, \t--vmname \tguestname"
     echo -e "-ssh, \t--ssh-location \tstream to extern server location"
     echo -e "-d, \t--destimation \tpath to backup location: /tmp"
-    echo -e "-ts, \t--timestamp \tcustom timestamp: +%F (use date --help for information)\n"
+    echo -e "-ts, \t--timestamp \tcustom timestamp: +%F (use date --help for information)"
+    echo -e "-ex, \t--exclude \texclude logical volume from backup \n"
 }
 
 function lvm-snap
@@ -56,6 +57,20 @@ function backup-ssh
 function check-lzop
 {
 	command -v lzop >/dev/null 2>&1 || { echo >&2 "I require lzop but it's not installed. Use \"apt-get install lzop\"  to solve this problem. Aborting.."; exit 1; }
+}
+
+function findDeviceArray
+{
+    for a in "${EXCLUDE_DEVICES[@]}"
+    do
+        cur_dev=`echo $1 | cut -d"/" -f4`
+        if [ "$a" == "$cur_dev" ]
+        then
+            return 0
+        fi
+    done
+
+    return 1
 }
 
 control_c()
@@ -103,6 +118,9 @@ do
    -ts  | --timestamp )   shift
 			  			  TS=$1
                           ;;
+   -ex  | --exclude )     shift
+			  			  EX_DEVs=$1
+                          ;;
    -h  | --help )         usage
                           exit
                 	  ;;
@@ -119,21 +137,33 @@ STRING=`virsh dumpxml $VMNAME | xmllint --xpath '/domain/devices/disk/source/@de
 
 IFS=', ' read -a DEVICES <<< "$STRING"
 
+# set exclude devices
+if [ -n "$EX_DEVs" ]; then
+    IFS=',' read -a EXCLUDE_DEVICES <<< "$EX_DEVs"
+fi
+
 # iterate devices
 for i in "${DEVICES[@]}"
 do
-   
-   lvm-snap $i
-   
-    if [ -n "$DEST" ]; then # do local backup
-   		backup-local $i
+
+   if findDeviceArray $i; then
+
+    echo -e "[WARN] Skipping $i"
+    continue
+
+   else
+       lvm-snap $i
+
+       if [ -n "$DEST" ]; then # do local backup
+           backup-local $i
+       fi
+
+       if [ -n "$BACKUPSSH" ]; then # do ssh backup
+           backup-ssh $i
+       fi
+
+       lvm-snap-remove $i
     fi
-   
-    if [ -n "$BACKUPSSH" ]; then # do ssh backup
-   		backup-ssh $i
-    fi
-   
-    lvm-snap-remove $i
    
 done
 
