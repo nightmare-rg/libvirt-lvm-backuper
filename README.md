01ba4b41a1b94e53ed99e2fe6ee853dea8f53b8d
joerg <nightmare@rising-gods.de>
2014-02-13 15:58:21 +0100

    added bandwidth limiting support with trickle

--- README.md
+++ README.md
@@ -4,12 +4,15 @@ libvirt-lvm-backuper
 lvm vm backup for libvirt
 
 
-usage: ./lvm-backuper.sh -vm vmname -d /tmp/ [-ssh "-p 22 user@example.com:/path/to/dest"] [-ts +%F] 
+usage: ./lvm-backuper.sh -vm vmname -d /tmp/ [-ssh "-p 22 user@example.com:/path/to/dest"] [-ts +%F] [-ex "storage01,storage02"] [-bw 2500]
 
--vm, 	--vmname 	guestname
-
--ssh, --ssh-location 	stream to extern server location
-
+-vm, 	--vmname 	    guestname
+-ssh,   --ssh-location 	stream to extern server location
 -d, 	--destimation 	path to backup location: /tmp
+-ts,	--timestamp	    custom timestamp: +%F (use date --help for information)
+-ex,    --exclude       exclude logical volume from backup"
+-bw,    --bandwidth     limit bandwidth for ssh upload (e.g. 2500 kb/s) \n"
 
--ts,	--timestamp	custom timestamp: +%F (use date --help for information)
+examples:
+
+for i in `virsh list | grep running | awk '{print $2}'`; do ./lvm-backuper.sh -vm $i -d /srv/lvm_backups/ -ts +%w; done
--- lvm-backuper.sh
+++ lvm-backuper.sh
@@ -3,7 +3,7 @@
 #
 #          FILE: lvm-backuper.sh
 # 
-#         USAGE: ./lvm-backuper.sh vm vmname -d /tmp/ [-ssh "-p 22 user@example.com:/path/to/dest"] [-ts +%F] [-ex "storage01,storage02"]
+#         USAGE: ./lvm-backuper.sh vm vmname -d /tmp/ [-ssh "-p 22 user@example.com:/path/to/dest"] [-ts +%F] [-ex "storage01,storage02"] [-bw 2500]
 #
 #        AUTHOR: Jörg Stewig (nightmare@rising-gods.de), 
 #===============================================================================
@@ -14,12 +14,13 @@
 
 function usage
 {
-    echo -e "\nusage: $0 -vm vmname -d /tmp/ [-ssh \"-p 22 user@example.com:/path/to/dest\"] [-ts +%F] [-ex \"storage01,storage02\"] \n"
+    echo -e "\nusage: $0 -vm vmname -d /tmp/ [-ssh \"-p 22 user@example.com:/path/to/dest\"] [-ts +%F] [-ex \"storage01,storage02\"] [-bw 2500]\n"
     echo -e "-vm, \t--vmname \tguestname"
     echo -e "-ssh, \t--ssh-location \tstream to extern server location"
     echo -e "-d, \t--destimation \tpath to backup location: /tmp"
     echo -e "-ts, \t--timestamp \tcustom timestamp: +%F (use date --help for information)"
-    echo -e "-ex, \t--exclude \texclude logical volume from backup \n"
+    echo -e "-ex, \t--exclude \texclude logical volume from backup"
+    echo -e "-bw, \t--bandwidth \tlimit bandwidth for ssh upload (e.g. 2500 kb/s) \n"
 }
 
 function lvm-snap
@@ -51,12 +52,23 @@ function backup-ssh
 	sshCommand=`echo $BACKUPSSH | cut -d":" -f1`
 	sshFolder=`echo $BACKUPSSH | cut -d":" -f2`
 	virsh dumpxml $VMNAME | ssh $sshCommand "cat - > $sshFolder/${VMNAME}-`date $TS`.xml"
-	dd if=${1}_snap bs=4M | lzop -c | ssh $sshCommand "cat - > $sshFolder/${snapname}_snap-`date $TS`.img.lzo"
+
+	if [ -n "$BW" ]; then # limit bandwidth
+	    dd if=${1}_snap bs=4M | lzop -c | trickle -u $BW ssh $sshCommand "cat - > $sshFolder/${snapname}_snap-`date $TS`.img.lzo"
+	else
+	    dd if=${1}_snap bs=4M | lzop -c | ssh $sshCommand "cat - > $sshFolder/${snapname}_snap-`date $TS`.img.lzo"
+	fi
+
 }
 
 function check-lzop
 {
 	command -v lzop >/dev/null 2>&1 || { echo >&2 "I require lzop but it's not installed. Use \"apt-get install lzop\"  to solve this problem. Aborting.."; exit 1; }
+}
+
+function check-trickle
+{
+    command -v trickle >/dev/null 2>&1 || { echo >&2 "I require trickle for traffic shaping but it's not installed. Use \"apt-get install trickle\"  to solve this problem. Aborting.."; exit 1; }
 }
 
 function findDeviceArray
@@ -121,6 +133,9 @@ do
    -ex  | --exclude )     shift
 			  			  EX_DEVs=$1
                           ;;
+   -bw  | --bandwidth )   shift
+			  			  BW=$1
+                          ;;
    -h  | --help )         usage
                           exit
                 	  ;;
@@ -131,6 +146,11 @@ do
     esac
     shift
 done
+
+# check if trickle is installed if needed
+if [ -n "$BW" ]; then # limit bandwidth
+	    check-trickle
+fi
 
 # get devices
 STRING=`virsh dumpxml $VMNAME | xmllint --xpath '/domain/devices/disk/source/@dev' - | sed s/dev=\"//g | sed s/\"//g`
