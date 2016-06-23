73cc2393d1f25197d158f05cb6ab69471f7b917e
joerg <nightmare@rising-gods.de>
2016-06-23 10:55:00 +0200

    add -pb option for progressbar feature

--- lvm-backuper.sh
+++ lvm-backuper.sh
@@ -1,11 +1,11 @@
-#!/bin/bash - 
+#!/bin/bash -
 #===============================================================================
 #
-#          FILE: lvm-backuper.sh
-# 
-#         USAGE: ./lvm-backuper.sh vm vmname -d /tmp/ [-ssh "-p 22 user@example.com:/path/to/dest"] [-ts +%F] [-ex "storage01,storage02"] [-bw 2500]
+#   FILE: lvm-backuper.sh
 #
-#        AUTHOR: Jörg Stewig (nightmare@rising-gods.de), 
+#   USAGE: ./lvm-backuper.sh vm vmname -d /tmp/ [-pb] [-ssh "-p 22 user@example.com:/path/to/dest"] [-ts +%F] [-ex "storage01,storage02"] [-bw 2500]
+#
+#   AUTHOR: Jörg Stewig (nightmare@rising-gods.de),
 #===============================================================================
 
 #===============================================================================
@@ -14,75 +14,100 @@
 
 function usage
 {
-    echo -e "\nusage: $0 -vm vmname -d /tmp/ [-ssh \"-p 22 user@example.com:/path/to/dest\"] [-ts +%F] [-ex \"storage01,storage02\"] [-bw 2500]\n"
-    echo -e "-vm, \t--vmname \tguestname"
-    echo -e "-ssh, \t--ssh-location \tstream to extern server location"
-    echo -e "-d, \t--destimation \tpath to backup location: /tmp"
-    echo -e "-ts, \t--timestamp \tcustom timestamp: +%F (use date --help for information)"
-    echo -e "-ex, \t--exclude \texclude logical volume from backup"
-    echo -e "-bw, \t--bandwidth \tlimit bandwidth for ssh upload (e.g. 2500 kb/s) \n"
+  echo -e "\nusage: $0 -vm vmname -d /tmp/ [-pb] [-ssh \"-p 22 user@example.com:/path/to/dest\"] [-ts +%F] [-ex \"storage01,storage02\"] [-bw 2500]\n"
+  echo -e "-vm, \t--vmname \tguestname"
+  echo -e "-d, \t--destimation \tpath to backup location: /tmp"
+  echo -e "-pb, \t--progressbar \tshow progressbar"
+  echo -e "-ssh, \t--ssh-location \tstream to extern server location"
+  echo -e "-ts, \t--timestamp \tcustom timestamp: +%F (use date --help for information)"
+  echo -e "-ex, \t--exclude \texclude logical volume from backup"
+  echo -e "-bw, \t--bandwidth \tlimit bandwidth for ssh upload (e.g. 2500 kb/s) \n"
 }
 
 function lvm-snap
 {
-	echo "[INFO] creating snapshot for ${1}.."
-	snapname=`echo $1 | cut -d"/" -f4`
-	lvcreate -s -L 2G -n ${snapname}_snap $1
+  echo "[INFO] creating snapshot for ${1}.."
+  snapname=`echo $1 | cut -d"/" -f4`
+  lvcreate -s -L 2G -n ${snapname}_snap $1
 
 }
 
 function lvm-snap-remove
 {
-	echo "[INFO] removing snapshot for ${1}.."
-	lvremove -f ${1}_snap
+  echo "[INFO] removing snapshot for ${1}.."
+  lvremove -f ${1}_snap
 }
 
 function backup-local
 {
-	echo "[INFO] starting backup to local destination: ${DEST}.."
-	snapname=`echo $1 | cut -d"/" -f4`
-	virsh dumpxml $VMNAME > $DEST/${VMNAME}-`date $TS`.xml
-	dd if=${1}_snap bs=4M | lzop -c > $DEST/${snapname}_snap-`date $TS`.img.lzo
+  echo "[INFO] starting backup to local destination: ${DEST}.."
+  snapname=`echo $1 | cut -d"/" -f4`
+  virsh dumpxml $VMNAME > $DEST/${VMNAME}-`date $TS`.xml
+
+  if [ -n "$PROGRESS" ]; then # show progressbar
+  lvmsize=`lvs $1 -o LV_SIZE --noheadings --units g --nosuffix`
+  lvmsize_rounded=`printf "%.0f" $(echo $lvmsize | bc)`
+  dd if=${1}_snap bs=8M | pv -petrb -s${lvmsize_rounded}g  | lzop -c > $DEST/${snapname}_snap-`date $TS`.img.lzo
+else
+  dd if=${1}_snap bs=8M | lzop -c > $DEST/${snapname}_snap-`date $TS`.img.lzo
+fi
 }
 
 function backup-ssh
 {
-	echo "[INFO] starting backup to ssh destination: ${BACKUPSSH}.."
-	snapname=`echo $1 | cut -d"/" -f4`
-	sshCommand=`echo $BACKUPSSH | cut -d":" -f1`
-	sshFolder=`echo $BACKUPSSH | cut -d":" -f2`
-	virsh dumpxml $VMNAME | ssh $sshCommand "cat - > $sshFolder/${VMNAME}-`date $TS`.xml"
+  echo "[INFO] starting backup to ssh destination: ${BACKUPSSH}.."
+  snapname=`echo $1 | cut -d"/" -f4`
+  sshCommand=`echo $BACKUPSSH | cut -d":" -f1`
+  sshFolder=`echo $BACKUPSSH | cut -d":" -f2`
+  virsh dumpxml $VMNAME | ssh $sshCommand "cat - > $sshFolder/${VMNAME}-`date $TS`.xml"
 
-	if [ -n "$BW" ]; then # limit bandwidth
-	    dd if=${1}_snap bs=4M | lzop -c | trickle -u $BW ssh $sshCommand "cat - > $sshFolder/${snapname}_snap-`date $TS`.img.lzo"
-	else
-	    dd if=${1}_snap bs=4M | lzop -c | ssh $sshCommand "cat - > $sshFolder/${snapname}_snap-`date $TS`.img.lzo"
-	fi
+  if [ -n "$BW" ]; then # limit bandwidth
+  if [ -n "$PROGRESS" ]; then # show progressbar
+  lvmsize=`lvs $1 -o LV_SIZE --noheadings --units g --nosuffix`
+  lvmsize_rounded=`printf "%.0f" $(echo $lvmsize | bc)`
+  dd if=${1}_snap bs=8M | pv -petrb -s${lvmsize_rounded}g | lzop -c | trickle -u $BW ssh $sshCommand "cat - > $sshFolder/${snapname}_snap-`date $TS`.img.lzo"
+else
+  dd if=${1}_snap bs=8M | lzop -c | trickle -u $BW ssh $sshCommand "cat - > $sshFolder/${snapname}_snap-`date $TS`.img.lzo"
+fi
+else
+  if [ -n "$PROGRESS" ]; then # show progressbar
+  lvmsize=`lvs $1 -o LV_SIZE --noheadings --units g --nosuffix`
+  lvmsize_rounded=`printf "%.0f" $(echo $lvmsize | bc)`
+  dd if=${1}_snap bs=8M | pv -petrb -s${lvmsize_rounded}g | lzop -c | ssh $sshCommand "cat - > $sshFolder/${snapname}_snap-`date $TS`.img.lzo"
+else
+  dd if=${1}_snap bs=8M | lzop -c | ssh $sshCommand "cat - > $sshFolder/${snapname}_snap-`date $TS`.img.lzo"
+fi
+fi
 
 }
 
 function check-lzop
 {
-	command -v lzop >/dev/null 2>&1 || { echo >&2 "I require lzop but it's not installed. Use \"apt-get install lzop\"  to solve this problem. Aborting.."; exit 1; }
+  command -v lzop >/dev/null 2>&1 || { echo >&2 "I require lzop but it's not installed. Use \"apt-get install lzop\"  to solve this problem. Aborting.."; exit 1; }
 }
 
 function check-trickle
 {
-    command -v trickle >/dev/null 2>&1 || { echo >&2 "I require trickle for traffic shaping but it's not installed. Use \"apt-get install trickle\"  to solve this problem. Aborting.."; exit 1; }
+  command -v trickle >/dev/null 2>&1 || { echo >&2 "I require trickle for traffic shaping but it's not installed. Use \"apt-get install trickle\"  to solve this problem. Aborting.."; exit 1; }
+}
+
+function check-pv
+{
+  command -v pv >/dev/null 2>&1 || { echo >&2 "I require pv for progressbar but it's not installed. Use \"apt-get install pv\"  to solve this problem. Aborting.."; exit 1; }
 }
 
 function findDeviceArray
 {
-    for a in "${EXCLUDE_DEVICES[@]}"
-    do
-        cur_dev=`echo $1 | cut -d"/" -f4`
-        if [ "$a" == "$cur_dev" ]
-        then
-            return 0
-        fi
-    done
+  for a in "${EXCLUDE_DEVICES[@]}"
+  do
+    cur_dev=`echo $1 | cut -d"/" -f4`
+    if [ "$a" == "$cur_dev" ]
+    then
+      return 0
+    fi
+  done
 
-    return 1
+  return 1
 }
 
 control_c()
@@ -98,8 +123,8 @@ control_c()
 
 # check parameter count
 if [ "$#" -lt 3 ]; then
-usage
-exit 1
+  usage
+  exit 1
 fi
 
 # some defaults
@@ -117,39 +142,47 @@ trap control_c SIGINT
 # parameter while-loop
 while [ "$1" != "" ];
 do
-    case $1 in
-   -vm  | --vmname )     	  shift
-                          VMNAME=$1
-                	  ;;
-   -d  | --destination )      shift
-			  			  DEST=$1
-                          ;;
-   -ssh  | --ssh )        shift
-			  			  BACKUPSSH=$1
-                          ;;
-   -ts  | --timestamp )   shift
-			  			  TS=$1
-                          ;;
-   -ex  | --exclude )     shift
-			  			  EX_DEVs=$1
-                          ;;
-   -bw  | --bandwidth )   shift
-			  			  BW=$1
-                          ;;
-   -h  | --help )         usage
-                          exit
-                	  ;;
-   *)                     usage
-                          echo "The parameter $1 is not allowed"
-                          exit 1 # error
-                	  ;;
-    esac
-    shift
+  case $1 in
+    -vm  | --vmname )     	shift
+    VMNAME=$1
+    ;;
+    -d  | --destination )  shift
+    DEST=$1
+    ;;
+    -pb  | --progressbar )
+    PROGRESS=1
+    ;;
+    -ssh  | --ssh )        shift
+    BACKUPSSH=$1
+    ;;
+    -ts  | --timestamp )   shift
+    TS=$1
+    ;;
+    -ex  | --exclude )     shift
+    EX_DEVs=$1
+    ;;
+    -bw  | --bandwidth )   shift
+    BW=$1
+    ;;
+    -h  | --help )         usage
+    exit
+    ;;
+    *)                     usage
+    echo "The parameter $1 is not allowed"
+    exit 1 # error
+    ;;
+  esac
+  shift
 done
 
 # check if trickle is installed if needed
 if [ -n "$BW" ]; then # limit bandwidth
-	    check-trickle
+check-trickle
+fi
+
+# check if pv is installed if needed
+if [ -n "$PROGRESS" ]; then # show progressbar
+check-pv
 fi
 
 # get devices
@@ -159,32 +192,32 @@ IFS=', ' read -a DEVICES <<< "$STRING"
 
 # set exclude devices
 if [ -n "$EX_DEVs" ]; then
-    IFS=',' read -a EXCLUDE_DEVICES <<< "$EX_DEVs"
+  IFS=',' read -a EXCLUDE_DEVICES <<< "$EX_DEVs"
 fi
 
 # iterate devices
 for i in "${DEVICES[@]}"
 do
 
-   if findDeviceArray $i; then
+  if findDeviceArray $i; then
 
     echo -e "[WARN] Skipping $i"
     continue
 
-   else
-       lvm-snap $i
+  else
+    lvm-snap $i
 
-       if [ -n "$DEST" ]; then # do local backup
-           backup-local $i
-       fi
+    if [ -n "$DEST" ]; then # do local backup
+    backup-local $i
+  fi
 
-       if [ -n "$BACKUPSSH" ]; then # do ssh backup
-           backup-ssh $i
-       fi
+  if [ -n "$BACKUPSSH" ]; then # do ssh backup
+  backup-ssh $i
+fi
 
-       lvm-snap-remove $i
-    fi
-   
+lvm-snap-remove $i
+fi
+
 done
 
 echo -e "[INFO] LVM Backup done for $VMNAME \n"
